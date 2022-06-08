405a51861465b0048ac1d798a1ec239b94e460d5
Joshua Wu <jww2145@columbia.edu>
2022-06-07 21:28:16 -0400

    commit tree b450206bb4dfa22c3d340d8ba9ffeb46de68b51a

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+function changeText(){
+    const text = document.querySelector('#text')
+    text.textContent = "This is really cool!"
+}
+
+document.addEventListener("DOMContentLoaded", changeText)
